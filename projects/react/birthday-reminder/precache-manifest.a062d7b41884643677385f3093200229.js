fcd217dd27c561a4e3905407737c6d86b4befdb9
jose <josevwebpro@gmail.com>
2021-09-11 13:29:14 -0600

    initial commit

---
+++ projects/react/birthday-reminder/precache-manifest.a062d7b41884643677385f3093200229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b69fead6b55d77fbe3bde9686ee5c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d998c00935157e3816ed",
+    "url": "/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d998c00935157e3816ed",
+    "url": "/static/js/main.bbe213d0.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
