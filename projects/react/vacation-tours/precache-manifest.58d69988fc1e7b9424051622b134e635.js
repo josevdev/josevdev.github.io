0b59169ece45ada2be14e3957c9286816235595a
jose <josevwebpro@gmail.com>
2021-09-11 13:34:36 -0600

    initial commit

---
+++ projects/react/vacation-tours/precache-manifest.58d69988fc1e7b9424051622b134e635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d732277be55ce9899e6bc08cad61a286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d07a459bc18439d1a1",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d07a459bc18439d1a1",
+    "url": "/static/js/main.c9ab11eb.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
